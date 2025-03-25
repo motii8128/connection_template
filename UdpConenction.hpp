e7d5596cd9bb9a946b5cb6f01d55f0cc324ddc37
motii <108280115+motii8128@users.noreply.github.com>
2025-03-25 11:54:55 +0900

    Update UdpConenction.hpp

--- UdpConenction.hpp
+++ UdpConenction.hpp
@@ -5,7 +5,7 @@
 #include <EthernetUdp.h>
 #include <ArduinoJson.h>
 
-#define PACKET_BUFFER_SIZE 256
+#define PACKET_BUFFER_SIZE 1024
 
 struct Axis
 {
@@ -49,55 +49,65 @@ class EthernetHandler
   public:
   void setup()
   {
+    pinMode(LED_BUILTIN, OUTPUT);
     digitalWrite(LED_BUILTIN, HIGH);
-    delay(500);
     digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
+    // Ethernet.init(17);
+    // Serial.println("[EthernetHandler] Initialized Ethernet.");
 
-    Ethernet.init(17);
-    Serial.println("[EthernetHandler] Initialized Ethernet.");
-
-    byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0x05 };
-    IPAddress default_ip(192, 168, 11, 2);
+    // byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0x05 };
+    // IPAddress default_ip(192, 168, 11, 1);
     
-    Ethernet.begin(mac, default_ip);
+    // Ethernet.begin(mac, default_ip);
 
-    Serial.print("[EthernetHandler] Start Ethernet on Address : ");
-    Serial.println(Ethernet.localIP());
+    // Serial.print("[EthernetHandler] Start Ethernet on Address : ");
+    // Serial.println(Ethernet.localIP());
 
-    while(udp_.begin(64205) == 0)
-    {
-      Serial.print(".");
-    }
-    Serial.println("[EthernetHandler] Start UDP on port : 64205");
+    // while(udp_.begin(64205) == 0)
+    // {
+    //   Serial.print(".");
+    //   delay(50);
+    // }
+    // Serial.println("[EthernetHandler] Start UDP on port : 64205");
 
-    Serial.println("[EthernetHandler] EthernetHandler OK!!!!!!!");
+    // Serial.println("[EthernetHandler] EthernetHandler OK!!!!!!!");
 
     digitalWrite(LED_BUILTIN, HIGH);
   }
 
   void receive()
   {
-    for(int i = 0; i < PACKET_BUFFER_SIZE; i++)
-    {
-      buf_[i] = 0;
-    }
-    int packet_size = udp_.parsePacket();
-
-    if(packet_size > 0)
-    {
-      udp_.read(buf_, PACKET_BUFFER_SIZE);
-    }
+    // int i = 0;
+    // for(int i = 0; i < PACKET_BUFFER_SIZE; i++)
+    // {
+    //   buf_[i] = 0;
+    // }
+    // if(Serial.available())
+    // {
+    //   buf_[i] = Serial.read();
+    //   if(buf_[i] == '\n')
+    //   {
+    //     i = 0;
+    //     // Serial.println(buf_);
+    //   }
+    //   else
+    //   {
+    //     i++;
+    //   }
+    // }
   }
   
   Joy GetJoyData()
   {
     auto joy = Joy();
 
-    DeserializationError error = deserializeJson(doc_, buf_);
+    DeserializationError error = deserializeJson(doc_, Serial);
 
     if (error) {
-      Serial.print(F("deserializeJson() failed: "));
-      Serial.println(error.f_str());
+      // Serial.print(F("deserializeJson() failed: "));
+      // Serial.println(error.f_str());
+      Serial.println(buf_);
     }
 
     joy.left_stick.x = doc_["left_stick"]["x"];
@@ -106,14 +116,15 @@ class EthernetHandler
     joy.right_stick.y = doc_["right_stick"]["y"];
     joy.dpad.x = doc_["dpad"]["x"];
     joy.dpad.y = doc_["dpad"]["y"];
-    joy.btns.circle = doc_["circle"];
-    joy.btns.cross = doc_["cross"];
-    joy.btns.cube = doc_["cube"];
-    joy.btns.triangle = doc_["triangle"];
-    joy.btns.l1 = doc_["l1"];
-    joy.btns.l2 = doc_["l2"];
-    joy.btns.r1 = doc_["r1"];
-    joy.btns.r2 = doc_["r2"];
+    joy.btns.circle = doc_["btns"]["circle"];
+    joy.btns.cross = doc_["btns"]["cross"];
+    joy.btns.cube = doc_["btns"]["cube"];
+    joy.btns.triangle = doc_["btns"]["triangle"];
+    joy.btns.l1 = doc_["btns"]["l1"];
+    joy.btns.l2 = doc_["btns"]["l2"];
+    joy.btns.r1 = doc_["btns"]["r1"];
+    joy.btns.r2 = doc_["btns"]["r2"];
+
 
     return joy;
   }
